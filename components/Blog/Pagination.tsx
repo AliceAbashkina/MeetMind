4b52a1a638ee0652c77319850f907ad051dbdc4c
Rustam Siyarov <rustamsiyarov@yandex.ru>
2023-04-13 04:51:50 +0600

    Немного пофиксил блог

--- components/Blog/Pagination.tsx
+++ components/Blog/Pagination.tsx
@@ -5,7 +5,7 @@ import Stack from '@mui/material/Stack';
 export default function BlogPagination() {
   return (
     <Stack alignItems="center" sx={{ marginTop: '64px', marginBottom: '64px' }}>
-      <Pagination count={10} size="large"/>
+      <Pagination count={1} size="large"/>
     </Stack>
   );
 }
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,15 +1,65 @@
 import * as React from 'react';
+import Footer from '../../components/Blog/Footer';
+import Header from '../../components/Blog/Header';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const theme = createTheme();
 
 export default function Article({ article }) {
     return (
         <div>
-            <h1>{article.title}</h1>
-            <div>
-            {article.lead}
-            </div>
-            <div>
-            {article.content}
-            </div>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Container maxWidth="lg">
+                    <Header title="Blog" />
+                    <main>
+                        <Typography component='h1' sx={{ lineHeight: '76px', fontSize: '44px', fontWeight: '700', textAlign: 'left', color: '#514573', marginBottom: '26px', marginTop: '64px' }}>Блог / {article.title}</Typography>
+                        <Typography sx={{ fontWeight: '300', fontSize: '14px'}}>
+                            Автор: {article.author}, {article.date}
+                        </Typography>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                padding: '0 0 40.54% 0',
+                                borderRadius: '40px',
+                                overflow: 'hidden',
+                                marginBottom: '78px',
+                                marginTop: '38px' 
+                            }}
+                        >
+                            <img style={{
+                                objectFit: 'cover',
+                                width: '100%',
+                                height: '100%',
+                                position: 'absolute',
+                                left: '0',
+                                top: '0'
+                            }}
+                                src='https://source.unsplash.com/random' alt={article.imageText} />
+                        </Box>
+                        <Box
+                            sx={{
+                                lineHeight: '50px',
+                                fontWeight: '400',
+                                fontSize: '28px',
+                                paddingBottom: '120px',
+                                borderBottom: '2px solid #514573',
+                                marginBottom: '80px'
+                            }}
+                        >
+                            {article.content}
+                        </Box>
+                    </main>
+                    <Footer
+                        title="Footer"
+                        description="Something here to give the footer a purpose!"
+                    />
+                </Container>
+            </ThemeProvider>
         </div>
     );
 }
@@ -33,6 +83,7 @@ export const getStaticProps = async (context) => {
         revalidate: 1,
     };
 };
+
 export async function getStaticPaths() {
 
     // Вызываем внешнюю конечную точку API для получения постов
@@ -41,7 +92,7 @@ export async function getStaticPaths() {
         length: 10,
         search: "password",
     };
-    
+
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //prod delete
 
     const response = await fetch('https://89.110.53.157:19525/v1/listArticle', {
